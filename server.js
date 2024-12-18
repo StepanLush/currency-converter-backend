a884d12f378d105a1a6fa7298ce2f837c74703d7
StepanLush <lusickijstepan@gmail.com>
2024-12-18 14:13:02 +0300

    fix: add promClient lib

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const promClient = require('prom-client');
 require('dotenv').config();
 
 const app = express();
